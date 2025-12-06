8997febca7a7a0adccc46acc25580da21c71042e
wanfuc901 <phuc.pham.vst@gmail.com>
2025-12-06 19:30:51 +0700

    update to norrmal option

--- QLNhaThuoc/Program.cs
+++ QLNhaThuoc/Program.cs
@@ -16,7 +16,7 @@ namespace QL_NhaThuoc
         {
             ApplicationConfiguration.Initialize(); // cấu hình mặc định cho WinForms
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmBaoCaoTongHop()); // Form khởi động
+            Application.Run(new LuaChon()); // Form khởi động
         }
     }
 }
